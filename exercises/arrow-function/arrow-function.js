992406b70bef9108f1e9de02b05ab1c7f28e8df8
Nicolas FRIZZARIN <nicolas.frizzarin54@gmail.com>
2018-04-27 12:50:16 +0200

    (test arrow-function) feat: add test on arrow function

---
+++ exercises/arrow-function/arrow-function.js
@@ -0,0 +1,52 @@
+/**
+ * @name arrow-function#simpleValue
+ * @type {function}
+ * @description function which let to test if return simple value with arrow function has understood
+ * @param {string} name
+ * @returns {string}
+ */
+const simpleValue = (name) => name;
+
+/**
+ * @name arrow-function#objectValue
+ * @type {function}
+ * @description function which let to test if return object value with arrow function has understood
+ * @param {number} first
+ * @param {number} second
+ * @param {number} multiplicateur
+ * @returns {{doubleFirst: number, doubleSecond: number}}
+ */
+const objectValue = ({first, second}, multiplicateur) => ({
+  doubleFirst: first * multiplicateur,
+  doubleSecond: second * multiplicateur
+});
+
+/**
+ * @name arrow-function#caseOfThis
+ * @type {function}
+ * @description function which let to test if context of this has understood
+ * @param {array} num
+ * @returns {Array}
+ */
+const caseOfThis = (num) => {
+  const object = {
+    num,
+    even: [],
+    each() {
+      let that = this;
+      num.map((value) => {
+        if (value % 2 === 0) {
+          that.even.push(value);
+        }
+      });
+    },
+  };
+  object.each();
+  return object.even;
+};
+
+export default {
+  simpleValue,
+  objectValue,
+  caseOfThis,
+}
---
+++ test/arrow-function/arrow-function.spec.js
@@ -0,0 +1,25 @@
+import 'babel-polyfill';
+import Assert from 'assert';
+import ArrowFunction from '../../exercises/arrow-function/arrow-function';
+
+describe('ES6 Arrow Function', () => {
+
+  it('should return name', () => {
+    const name = 'Nicolas';
+    Assert.strictEqual(ArrowFunction.simpleValue(name), name);
+  });
+
+  it('should return {doubleFirst: 4, doubleSecond: 10}', () => {
+    const object = {
+      first: 2,
+      second: 5,
+    };
+    Assert.deepStrictEqual(ArrowFunction.objectValue(object, 2), {doubleFirst: 4, doubleSecond: 10});
+  });
+
+  it('should return [2,4]', () => {
+    const array = [1,2,3,4];
+    ArrowFunction.caseOfThis(array);
+    Assert.deepStrictEqual(ArrowFunction.caseOfThis(array), [2,4]);
+  });
+});
