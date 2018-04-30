7460ebb02bffe4cd0d01512fa00f2918f4993d9b
Nicolas FRIZZARIN <nicolas.frizzarin54@gmail.com>
2018-04-30 15:46:38 +0200

    (state iterator) feat: add state of the iterator concept

---
+++ exercises/symbol/symbol.js
@@ -0,0 +1,12 @@
+/**
+ * @type {function}
+ * @description Test if Symbol has understood
+ * @param object
+ * @returns {*[]}
+ */
+export default (object) => {
+  /**
+   * Add your code here
+   */
+  return [...object];
+};
