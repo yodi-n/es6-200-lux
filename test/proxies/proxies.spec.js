abee546fa2ce1a4f2c24d443c0777015799f906d
Nicolas FRIZZARIN <nicolas.frizzarin54@gmail.com>
2018-04-30 15:54:01 +0200

    (state proxies) feat: add state of the proxies concept

---
+++ test/proxies/proxies.spec.js
@@ -0,0 +1,28 @@
+import 'babel-polyfill';
+import Assert from 'assert';
+import { trap } from '../../exercises/proxies/proxies';
+
+describe('Proxies ES6 comprehension', () => {
+
+  it('should return the length of array == 2', () => {
+    let pseudos = [];
+    let pseudosProxy = new Proxy(pseudos, trap);
+    pseudosProxy.push('Nicolas', 'SFEIR', 'Nicolas');
+    Assert.deepStrictEqual(pseudosProxy.length, 2);
+    Assert.deepStrictEqual(pseudos.length, 2);
+  });
+
+  it('should return nicolas instead of Nicolas', () => {
+    let pseudos = [];
+    let pseudoProxies = new Proxy(pseudos, trap);
+    pseudoProxies.push('Nicolas');
+    Assert.strictEqual(pseudoProxies[0], 'nicolas');
+  });
+
+  it('should return string without special character', () => {
+    let pseudos = [];
+    let pseudoProxies = new Proxy(pseudos, trap);
+    pseudoProxies.push('iMa_Ro%%oR_P3wn€R');
+    Assert.strictEqual(pseudoProxies[0], 'ima_roor_p3wnr');
+  });
+});
