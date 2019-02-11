f47a2715ed2d94168847489992510ae06b87e61c
yodi-n <yodi.n@sfeir.com>
2019-02-11 16:52:01 +0100

    Fix async/await test

--- test/async-await/async-await.spec.js
+++ test/async-await/async-await.spec.js
@@ -1,30 +1,16 @@
 import 'babel-polyfill';
 import Assert from 'assert';
-import { trap } from '../../exercises/proxies/proxies';
+import GetUser from '../../exercises/async-await/async-await';
 
-// This file must not be modified => thanks for doing theses exercises correctly and seriously ;)
+describe('Async/Await ES6 Comprehension', () => {
 
-describe('Proxies ES6 comprehension', () => {
-
-  it('should return the length of array == 2', () => {
-    let pseudos = [];
-    let pseudosProxy = new Proxy(pseudos, trap);
-    pseudosProxy.push('Nicolas', 'SFEIR', 'Nicolas');
-    Assert.deepStrictEqual(pseudosProxy.length, 2);
-    Assert.deepStrictEqual(pseudos.length, 2);
-  });
-
-  it('should return nicolas instead of Nicolas', () => {
-    let pseudos = [];
-    let pseudoProxies = new Proxy(pseudos, trap);
-    pseudoProxies.push('Nicolas');
-    Assert.strictEqual(pseudoProxies[0], 'nicolas');
-  });
-
-  it('should return string without special character', () => {
-    let pseudos = [];
-    let pseudoProxies = new Proxy(pseudos, trap);
-    pseudoProxies.push('iMa_Ro%%oR_P3wn€R');
-    Assert.strictEqual(pseudoProxies[0], 'ima_roor_p3wnr');
+    it('should return an array', async () => {
+        let myResponse;
+        try {
+            myResponse = await GetUser();
+        } catch (error) {
+            console.log(error);
+        }
+        Assert.equal(myResponse.userId, 1);
   });
 });
